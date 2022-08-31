01bc16651d58b2c05e8542209989a8d2a24af5d0
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-08-31 14:21:22 +0900

    Add files via upload

---
+++ C#/2557.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
