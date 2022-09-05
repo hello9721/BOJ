4015fcb6c1d2597e1732406bdd56b27eb70a3ee6
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-05 13:37:47 +0900

    Add files via upload

---
+++ C#/10952.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+while (true)
+{
+    string T = Console.ReadLine();
+
+    if (T == "0 0")
+    {
+        break;
+    }
+    else
+    {
+        int a = int.Parse(T.Split(" ")[0]);
+        int b = int.Parse(T.Split(" ")[1]);
+
+        Console.WriteLine(a + b);
+    }
+}
