75fa4154d977a5591f4f7f5187b8313881ca4dab
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-16 15:46:56 +0900

    Add files via upload

---
+++ boj Question name checker.py
@@ -0,0 +1,54 @@
+b = """
+Sprout 1000
+A+B
+ 
+STANDARD
+199,795
+2.40
+Sprout 1001
+A-B
+ 
+STANDARD
+168,665
+1.41
+Sprout 1008
+A/B
+ 
+STANDARD
+131,996
+2.86
+Bronze V 1271
+엄청난 부자2
+"""
+
+# 위의 데이터는 예시 입니다.
+# 지우고 넣으시면 됩니다.
+
+b = b.split("\n")
+    
+num = []
+name = []
+
+for i in range(len(b)):
+    if "Bronze" in b[i] or "Silver" in b[i]or "Gold" in b[i] or "Platinum" in b[i] or "Diamond" in b[i]  or "Ruby" in b[i]:
+        k = b[i].split()
+        num.append(k[2])
+        name.append(b[i+1])
+        
+    elif "Sprout" in b[i]:
+        k = b[i].split()
+        num.append(k[1])
+        name.append(b[i+1])  
+
+while 1:
+    try:
+        c =  input()
+
+        if c == "0":
+            break
+        
+        n = num.index(c)
+        print(f"{num[n]} {name[n]}.py")
+    except:
+        print(f"\n[ {c} ] 는 목록에 없습니다. 다시 입력해주세요.\n")
+        continue
