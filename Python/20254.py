3a4f3b8b4396ae67b67d7f429e62eca8737acdf1
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-16 13:00:46 +0900

    Add files via upload

---
+++ Python/20254.py
@@ -0,0 +1,5 @@
+UR, TR, UO, TO = map(int, input().split())
+
+a = 56*UR + 24*TR + 14*UO + 6*TO
+
+print(a)
---
+++ Python/20492.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+x = n / 100
+y = n - ( x * 80)
+
+a = n - (x * 22)
+
+b = x * 80 + (y - y / 100 * 22)
+
+
+print(int(a),int(b))
